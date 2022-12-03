22013c867ad58a5d3a55186ba896b8cb0fe3bf0e
Gleb <vanechka_samoylov_90@mail.ru>
2022-12-03 09:11:24 +0300

    close issue #2

--- main/main.py
+++ main/main.py
@@ -24,6 +24,58 @@ def search_film(name):
 
     return scraped
 
+def search_full(url):
+    global director
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, "html.parser")
+
+    moviesGenre = soup.find('p', class_='scoreboard__info')
+    cons = soup.find('p', class_='what-to-know__section-body')
+    dirs = soup.findAll('li', class_='meta-row clearfix')
+
+    scraped = []
+
+    if moviesGenre is not None:
+        scraped.append(moviesGenre.text)
+    else:
+        scraped.append('Отсутствует информация')
+    if cons is not None:
+        scraped.append(cons.text)
+    else:
+        scraped.append('Отсутствует информация')
+    if dirs is not None:
+        for dir in dirs:
+            if dir.text.__contains__('Director'):
+                director = dir.text.strip()
+                director = director.replace("\n", "")
+                director = director.replace("\r", "")
+                director = director.replace("Director:", "")
+        scraped.append(director)
+    else:
+        scraped.append('Отсутствует информация')
+
+    return scraped
+
+def search_actors(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, "html.parser")
+
+    actors = soup.findAll('div', class_='cast-item media inlineBlock')
+    scraped = []
+    ss = ''
+
+    for actor in actors:
+        act = actor.find('div', class_='media-body').a
+        if act is not None:
+            scraped.append(act)
+            ss += str(act.text) + " "
+
+    if ss == '':
+        ss = 'Отсутствует информация'
+
+    ss = ss.replace("\r", "")
+    ss = ss.replace("\n", "")
+    return ss.strip()
 
 # bot
 bot = telebot.TeleBot("5617127293:AAFNjtGv6hQfj4ohxJV3sOQLAXr9gEJeqjg")
@@ -75,6 +127,11 @@ def get_var_names(message):
 
 def get_prom(message):
 
+    global parameters
+    global actors
+    parameters = search_full(cor_film)
+    actors = search_actors(cor_film)
+
     keyboard2 = types.InlineKeyboardMarkup()  # наша клавиатура
     key1 = types.InlineKeyboardButton(text='Выборочная информация', callback_data='14')
     keyboard2.add(key1)
@@ -87,6 +144,22 @@ def get_prom(message):
     question = 'Что ты хочешь узнать об этом фильме?'
     bot.send_message(message.from_user.id, text=question, reply_markup=keyboard2)
 
+
+def get_param(message):
+
+    keyboard2 = types.InlineKeyboardMarkup()  # наша клавиатура
+    key1 = types.InlineKeyboardButton(text='Год, Жанр, Продолжительность', callback_data='6')
+    keyboard2.add(key1)
+    key4 = types.InlineKeyboardButton(text='Что говорят критики', callback_data='9')
+    keyboard2.add(key4)
+    key5 = types.InlineKeyboardButton(text='Режиссеры', callback_data='10')
+    keyboard2.add(key5)
+    key7 = types.InlineKeyboardButton(text='Актеры', callback_data='12')
+    keyboard2.add(key7)
+    key8 = types.InlineKeyboardButton(text='Назад', callback_data='13')
+    keyboard2.add(key8)
+    question = 'Что конкретно ты хочешь узнать об этом фильме?'
+    bot.send_message(message.from_user.id, text=question, reply_markup=keyboard2)
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
@@ -111,6 +184,23 @@ def callback_worker(call):
         bot.send_message(call.message.chat.id, film[4][2])
         cor_film = film[4][2]
         bot.register_next_step_handler(call.message, get_prom)
+    elif call.data == "6":
+        bot.send_message(call.message.chat.id, parameters[0])
+    elif call.data == "9":
+        bot.send_message(call.message.chat.id, parameters[1])
+    elif call.data == "10":
+        bot.send_message(call.message.chat.id, parameters[2])
+    elif call.data == "12":
+        bot.send_message(call.message.chat.id, actors)
+    elif call.data == "13":
+        bot.register_next_step_handler(call.message, get_prom)
+    elif call.data == "14":
+        bot.register_next_step_handler(call.message, get_param)
+
+    elif call.data == "16":
+        bot.register_next_step_handler(call.message, get_var_names)
+    elif call.data == "17":
+        bot.register_next_step_handler(call.message, start)
 
 
 bot.infinity_polling()
